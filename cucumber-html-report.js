48b8f2ed661e4a14c4161fc43530e17e795101fa
lvansav <vansav.ka@gmail.com>
2022-08-28 12:33:34 +0300

    Added html cucumber  reportrer to project

---
+++ cucumber-html-report.js
@@ -0,0 +1,24 @@
+const report = require('multiple-cucumber-html-reporter')
+
+report.generate({
+    jsonDir: "jsonlogs",
+    reportPath: "./reports/cucumber-htmlreport.html",
+    metadata: {
+        browser: {
+            name: 'chrome',
+            version: 'XX'
+        },
+        device: 'Local test machine',
+        platform: {
+            name:'ubuntu',
+            version: 'latest'
+        }
+    },
+    customData: {
+        title: "Run info",
+        data: [
+            { label: 'Project', value: 'Tenyx website testing' },
+            { label: 'Execution date', value: new Date().toISOString().slice(0, 10) }
+        ]
+    }
+})
